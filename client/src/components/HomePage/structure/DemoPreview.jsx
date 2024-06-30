70a288688247cd7feed936963c376e52bffcaff7
Antoan Metodiev <Boksnem@abv.bg>
2024-07-01 00:21:52 +0300

    add games section and add 3 models

--- client/src/components/HomePage/structure/DemoPreview.jsx
+++ client/src/components/HomePage/structure/DemoPreview.jsx
@@ -14,6 +14,10 @@ import LenovoLegion from "../resources/images/lenovo legion.jpg";
 import AsusTuf from "../resources/images/asus tuf.png";
 import AlienWareLaptop from "../resources/images/alienware laptop.jpg";
 
+import Ps5Game from "../resources/images/ps5 game.jpg";
+import Ps4Game from "../resources/images/the last of us.jpg";
+import XboxGame from "../resources/images/forza horizon.jpg";
+
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faP } from "@fortawesome/free-solid-svg-icons";
 
@@ -25,6 +29,47 @@ export const DemoPreview = () => {
   return (
     <section className="demo-website-preview">
 
+      {/* Games */}
+      <h2>Games</h2>
+
+      <article className="playstation">
+        <div className="playstation _5">
+          <img
+            className="playstation 5_img"
+            src={Ps5Game}
+            alt="Playstation 5 image"
+          />
+          <div className="content">
+            <h2>PS5</h2>
+            <button>VIEW NOW</button>
+          </div>
+          <span className="bkg-color"></span>
+        </div>
+        <div className="playstation _5">
+          <img
+            className="playstation 5_img"
+            src={Ps4Game}
+            alt="Playstation 5 image"
+          />
+          <div className="content">
+            <h2>PS4</h2>
+            <button>VIEW NOW</button>
+          </div>
+          <span className="bkg-color"></span>
+        </div>
+        <div className="playstation _5">
+          <img
+            className="playstation 5_img"
+            src={XboxGame}
+            alt="Playstation 5 image"
+          />
+          <div className="content">
+            <h2>XBOX</h2>
+            <button>VIEW NOW</button>
+          </div>
+          <span className="bkg-color"></span>
+        </div>
+      </article>
 
       {/* PC */}
       <h2>PC</h2>
