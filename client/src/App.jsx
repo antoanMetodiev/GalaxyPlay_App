973b0b053cb102e681720b8d66227e19b6edc136
Antoan Metodiev <Boksnem@abv.bg>
2024-06-30 16:34:36 +0300

    separated header code in a component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,10 @@
-
+import { HomePage } from "./components/HomePage/HomePage";
 
 function App() {
 
 	return (
 	<>
-		<header className="site-header">
-        <div className="logo-data-wrapper">
-          <img src={LastLogo} alt="GalaxyPlay-Logo" />
-          <h1 className="site-title">GalaxyPlay</h1>
-		  <span className="title-border"></span>
-        </div>
-        <nav className="header-nav">
-          {/* TODO: Replace with Link components */}
-          <ul>
-            <a href="#">Sign In</a>
-            <a href="#">Register</a>
-          </ul>
-          <ul>
-            <a href="#">For Us</a>
-            <a href="#">Contacts</a>
-          </ul>
-        </nav>
-      </header> 
+		<HomePage />
 	</>
 	);
 }
