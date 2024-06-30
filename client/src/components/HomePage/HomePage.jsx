6591784abb45fce3d5036bc270a8068c4b0002d9
Antoan Metodiev <Boksnem@abv.bg>
2024-06-30 16:40:33 +0300

    add HomePage first section

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -5,24 +5,44 @@ export const HomePage = () => {
   
     return (
       <>
-        <header className="site-header">
-        <div className="logo-data-wrapper">
-          <img src={LastLogo} alt="GalaxyPlay-Logo" />
-          <h1 className="site-title">GalaxyPlay</h1>
-		  <span className="title-border"></span>
-        </div>
-        <nav className="header-nav">
-          {/* TODO: Replace with Link components */}
-          <ul>
-            <a href="#">Sign In</a>
-            <a href="#">Register</a>
-          </ul>
-          <ul>
-            <a href="#">For Us</a>
-            <a href="#">Contacts</a>
-          </ul>
-        </nav>
-      </header>            
+          <header className="site-header">
+          <div className="logo-data-wrapper">
+            <img src={LastLogo} alt="GalaxyPlay-Logo" />
+            <h1 className="site-title">GalaxyPlay</h1>
+            <span className="title-border"></span>
+          </div>
+          <nav className="header-nav">
+            {/* TODO: Replace with Link components */}
+            <ul>
+              <a href="#">Sign In</a>
+              <a href="#">Register</a>
+            </ul>
+            <ul>
+              <a href="#">For Us</a>
+              <a href="#">Contacts</a>
+            </ul>
+          </nav>
+        </header>
+
+        <section className="first section">
+          <video
+            className="background-clip"
+            src={allVideos[currentIndex]}
+            autoPlay
+            loop
+            muted
+          ></video>
+          <FontAwesomeIcon
+            onClick={onRightWallperHandler}
+            className="on-right-icon"
+            icon={faAnglesRight}
+          />
+          <FontAwesomeIcon
+            onClick={onLeftWallperHandler}
+            className="on-left-icon"
+            icon={faAnglesLeft}
+          />
+        </section>      
       </>
     );
   };
