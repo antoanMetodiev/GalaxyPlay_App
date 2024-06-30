14eb2bf8536e49ccaf5e1cd5f5c89ab5fe2c9adf
Antoan Metodiev <Boksnem@abv.bg>
2024-06-30 22:32:57 +0300

    add xbox section and xbox consoles

--- client/src/components/HomePage/structure/DemoPreview.jsx
+++ client/src/components/HomePage/structure/DemoPreview.jsx
@@ -1,5 +1,8 @@
 import Playstation5Image from '../resources/images/ps-5.webp';
-import Playstation4Image from '../resources/images/ps4 pro.jpg';
+import Playstation4ProImage from '../resources/images/ps4 pro.jpg';
+import Playstation4SlimImage from '../resources/images/ps4 slim.jpg';
+
+// import XboxX from
 
 export const DemoPreview = () => {
 
@@ -12,28 +15,57 @@ export const DemoPreview = () => {
           <h2>Playstation</h2>
 
           <article className="playstation">
-            
             <div className="playstation _5">
               <img className="playstation 5_img" src={Playstation5Image} alt="Playstation 5 image" />
               <div className='content'>
                 <h2>Playstation 5</h2>
-                <button>BUY NOW</button>
+                <button>VIEW NOW</button>
               </div>
               <span className='bkg-color'></span>
             </div>
             <div className="playstation _5">
-              <img className="playstation 5_img" src={Playstation5Image} alt="Playstation 5 image" />
+              <img className="playstation 5_img" src={Playstation4ProImage} alt="Playstation 5 image" />
               <div className='content'>
-                <h2>Playstation 5</h2>
-                <button>BUY NOW</button>
+                <h2>Playstation 4 Pro</h2>
+                <button>VIEW NOW</button>
               </div>
               <span className='bkg-color'></span>
             </div>
             <div className="playstation _5">
+              <img className="playstation 5_img" src={Playstation4SlimImage} alt="Playstation 5 image" />
+              <div className='content'>
+                <h2>Playstation 4 Slim</h2>
+                <button>VIEW NOW</button>
+              </div>
+              <span className='bkg-color'></span>
+            </div>
+          </article>
+
+          {/* XBOX */}
+          <h2>XBOX</h2>
+
+          <article className="playstation">
+            <div className="playstation _5">
               <img className="playstation 5_img" src={Playstation5Image} alt="Playstation 5 image" />
               <div className='content'>
-                <h2>Playstation 5</h2>
-                <button>BUY NOW</button>
+                <h2>XBOX X</h2>
+                <button>VIEW NOW</button>
+              </div>
+              <span className='bkg-color'></span>
+            </div>
+            <div className="playstation _5">
+              <img className="playstation 5_img" src={Playstation4ProImage} alt="Playstation 5 image" />
+              <div className='content'>
+                <h2>Playstation 4 Pro</h2>
+                <button>VIEW NOW</button>
+              </div>
+              <span className='bkg-color'></span>
+            </div>
+            <div className="playstation _5">
+              <img className="playstation 5_img" src={Playstation4SlimImage} alt="Playstation 5 image" />
+              <div className='content'>
+                <h2>Playstation 4 Slim</h2>
+                <button>VIEW NOW</button>
               </div>
               <span className='bkg-color'></span>
             </div>
