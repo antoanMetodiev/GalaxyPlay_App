515e820bb77b2fb7c23b1643bd1b94defa877d1c
Antoan Metodiev <Boksnem@abv.bg>
2024-06-30 16:50:13 +0300

    add arrows functionality
    аdd arrows to change the Live Wallpaper (previous and next)

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -23,64 +23,73 @@ import Video10 from "./videos/pubg.mp4";
 let allVideos = [Video0, Video1, Video2, Video3, Video4, Video5, Video6, Video7, Video8, Video9, Video10,];
 
 function getRandomIntInclusive(min, max) {
-	min = Math.floor(min);  // because can be double number
-	max = Math.max(max); 
-	// TODO: This function is not working soo good, maybe
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
+  min = Math.floor(min);  // because can be double number
+  max = Math.max(max); 
+  // TODO: This function is not working soo good, maybe
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 // HomePage Component
 import { useEffect, useState } from "react";
 
 export const HomePage = () => {
+  const [currentIndex, setCurentIndex] = useState(0);
 
-	const [currentIndex, setCurentIndex] = useState(0);
+  useEffect(() => {
+    const result = getRandomIntInclusive(0, allVideos.length - 1);
+    setCurentIndex(result);
+  }, []);
 
-	useEffect(() => {
-		const result = getRandomIntInclusive(0, allVideos.length - 1);
-		setCurentIndex(result);
-	}, []);
-  
-    return (
-      <>
-          <header className="site-header">
-          <div className="logo-data-wrapper">
-            <img src={LastLogo} alt="GalaxyPlay-Logo" />
-            <h1 className="site-title">GalaxyPlay</h1>
-            <span className="title-border"></span>
-          </div>
-          <nav className="header-nav">
-            {/* TODO: Replace with Link components */}
-            <ul>
-              <a href="#">Sign In</a>
-              <a href="#">Register</a>
-            </ul>
-            <ul>
-              <a href="#">For Us</a>
-              <a href="#">Contacts</a>
-            </ul>
-          </nav>
-        </header>
+  function onRightWallperHandler() {
+    if (currentIndex + 1 >= allVideos.length) setCurentIndex(0);
+    else setCurentIndex(currentIndex + 1);
+  }
 
-        <section className="first section">
-          <video
-            className="background-clip"
-            src={allVideos[currentIndex]}
-            autoPlay
-            loop
-            muted
-          ></video>
-          <FontAwesomeIcon
-            onClick={onRightWallperHandler}
-            className="on-right-icon"
-            icon={faAnglesRight}
-          />
-          <FontAwesomeIcon
-            onClick={onLeftWallperHandler}
-            className="on-left-icon"
-            icon={faAnglesLeft}
-          />
-        </section>      
-      </>
-    );
-  };
+  function onLeftWallperHandler() {
+    if (currentIndex - 1 < 0) setCurentIndex(allVideos.length - 1);
+    else setCurentIndex(currentIndex - 1);
+  }
+
+  return (
+    <>
+      <header className="site-header">
+        <div className="logo-data-wrapper">
+          <img src={LastLogo} alt="GalaxyPlay-Logo" />
+          <h1 className="site-title">GalaxyPlay</h1>
+		  <span className="title-border"></span>
+        </div>
+        <nav className="header-nav">
+          {/* TODO: Replace with Link components */}
+          <ul>
+            <a href="#">Sign In</a>
+            <a href="#">Register</a>
+          </ul>
+          <ul>
+            <a href="#">For Us</a>
+            <a href="#">Contacts</a>
+          </ul>
+        </nav>
+      </header>
+
+      <section className="first section">
+        <video
+          className="background-clip"
+          src={allVideos[currentIndex]}
+          autoPlay
+          loop
+          muted
+        ></video>
+        <FontAwesomeIcon
+          onClick={onRightWallperHandler}
+          className="on-right-icon"
+          icon={faAnglesRight}
+        />
+        <FontAwesomeIcon
+          onClick={onLeftWallperHandler}
+          className="on-left-icon"
+          icon={faAnglesLeft}
+        />
+      </section>
+    </>
+  );
+};
