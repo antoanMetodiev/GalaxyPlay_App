643b671e60735c1f975f9b0741567e7ab2574c92
Antoan Metodiev <Boksnem@abv.bg>
2024-06-30 16:30:49 +0300

    create HomePage navigation

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+
+
+function App() {
+
+	return (
+	<>
+		<header className="site-header">
+        <div className="logo-data-wrapper">
+          <img src={LastLogo} alt="GalaxyPlay-Logo" />
+          <h1 className="site-title">GalaxyPlay</h1>
+		  <span className="title-border"></span>
+        </div>
+        <nav className="header-nav">
+          {/* TODO: Replace with Link components */}
+          <ul>
+            <a href="#">Sign In</a>
+            <a href="#">Register</a>
+          </ul>
+          <ul>
+            <a href="#">For Us</a>
+            <a href="#">Contacts</a>
+          </ul>
+        </nav>
+      </header> 
+	</>
+	);
+}
+
+export default App;
---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,26 @@
+export const HomePage = () => {
+    
+  
+    return (
+      <>
+        <header className="site-header">
+        <div className="logo-data-wrapper">
+          <img src={LastLogo} alt="GalaxyPlay-Logo" />
+          <h1 className="site-title">GalaxyPlay</h1>
+		  <span className="title-border"></span>
+        </div>
+        <nav className="header-nav">
+          {/* TODO: Replace with Link components */}
+          <ul>
+            <a href="#">Sign In</a>
+            <a href="#">Register</a>
+          </ul>
+          <ul>
+            <a href="#">For Us</a>
+            <a href="#">Contacts</a>
+          </ul>
+        </nav>
+      </header>            
+      </>
+    );
+  };
