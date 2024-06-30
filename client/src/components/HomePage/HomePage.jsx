e3d30311d7e0a9ae8807a775b2989a1ff0ee2acc
Antoan Metodiev <Boksnem@abv.bg>
2024-06-30 17:02:40 +0300

    separate resources in folder

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -1,26 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAnglesRight, faAnglesLeft } from "@fortawesome/free-solid-svg-icons";
 import "../HomePage/HomePage.css";
-import LastLogo from "./images/last-logo.jfif";
-
-// TODO: Tova trqbwa da se iznese vuv funshen file:
-// VIDEOS
-import Video0 from "./videos/call of duty.mp4";
-import Video1 from "./videos/god of war.mp4";
-import Video2 from "./videos/Scorpion - Mortal Combat.mp4";
-import Video3 from "./videos/spider man - falling.mp4";
-import Video4 from "./videos/Ghost - Call of Duty.mp4";
-import Video5 from "./videos/Ghost-of-Tsushima.mp4";
-import Video6 from "./videos/Grand-Theft-Auto-VI.mp4";
-import Video7 from "./videos/Acc creed Origins.mp4";
-import Video8 from "./videos/Need-For-Speed.mp4";
-import Video9 from "./videos/fortnite.mp4";
-import Video10 from "./videos/pubg.mp4";
-// -----------------------------------------------
-
-// games for add: Gta 5, nqkakuv Hunter, Minecraft, CS-GO, Red Dead Redemption, Santinela LOL
-
-let allVideos = [Video0, Video1, Video2, Video3, Video4, Video5, Video6, Video7, Video8, Video9, Video10,];
 
 function getRandomIntInclusive(min, max) {
   min = Math.floor(min);  // because can be double number
@@ -29,7 +9,9 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// HomePage Component
+// HomePage Component:
+import LastLogo from "./resources/images/last-logo.jfif";
+import { allVideos } from "./resources/videos";
 import { useEffect, useState } from "react";
 
 export const HomePage = () => {
