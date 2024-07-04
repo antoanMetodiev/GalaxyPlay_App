2eab540d1d65d7a31734f71518489b70d0eb4ec2
Antoan Metodiev <Boksnem@abv.bg>
2024-07-04 21:18:48 +0300

    when user click on button - navigate to login page

--- client/src/components/Register-Login_Page/Login.jsx
+++ client/src/components/Register-Login_Page/Login.jsx
@@ -2,6 +2,7 @@ import styles from "./Register_Login.module.css";
 import { useForm } from "./hooks/useForm";
 
 import backgroundVideo from "./videos/login wallper.mp4";
+import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
   const [formValues, onChangeHandler, onSubmitHandler] = useForm({
@@ -9,7 +10,10 @@ export const Login = () => {
     password: "",
   });
 
-  console.log(formValues);
+  const navigate = useNavigate();
+  const onClickSignUpHandler = () => {
+    navigate('/register');
+  }
 
   return (
     <div className={styles["register-page-container"]}>
@@ -23,26 +27,27 @@ export const Login = () => {
       >
         Your browser does not support the video tag.
       </video>
-      <form onSubmit={onSubmitHandler} className={styles["register"]}>
+      <form onSubmit={onSubmitHandler} className={`${styles["register"]} ${styles["other"]}}`}>
         <header className={styles["header"]}>
-          <h1>Login</h1>
+          <h1>Sign In</h1>
         </header>
         <fieldset>
           <legend>Username &amp; Email:</legend>
           <div className={`${styles["field"]} ${styles["text"]} ${styles["icon-username"]}`}>
-            <input onChange={onChangeHandler} name="username" type="text" id="username" required="" />
+            <input onChange={onChangeHandler} name="username" type="text" id="username" placeholder="Username..."/>
             <i className="fa fa-user" />
             <label htmlFor="username">Username: </label>
             <span className={styles["helper"]}>Hello there</span>
           </div>
           <div className={`${styles["field"]} ${styles["text"]} ${styles["icon-password"]}`}>
-            <input onChange={onChangeHandler} name="password" type="password" id="re-password" />
+            <input onChange={onChangeHandler} name="password" type="password" id="re-password"  placeholder="Password..."/>
             <label htmlFor="re-password">Password:</label>
             <i className="fa fa-key" />
           </div>
         </fieldset>
 
-        <input type="submit" value="Login" />
+        <span onClick={onClickSignUpHandler} className={styles["sign-up-option"]}>Sign Up</span>
+        <input type="submit" value="Sign In" />
       </form>
     </div>
   );
