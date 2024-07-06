52dd4e84afc23fcdbaf5022e1c2cc2abcb56bf40
Antoan Metodiev <Boksnem@abv.bg>
2024-07-06 18:47:35 +0300

    add Ps5Games_Page component

---
+++ client/src/components/Categories/Games/Ps5Games_Page/Ps5Games_Page.jsx
@@ -0,0 +1,18 @@
+import { Header } from "./structure/Header/Header";
+import { Body } from "./structure/Body/Body";
+
+export const Ps5Games_Page = () => {
+    
+
+
+    return (
+
+        <>
+            <Header />
+            <Body />
+        </>
+
+
+        
+    );
+}
