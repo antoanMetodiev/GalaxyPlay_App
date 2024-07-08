fe0023f3f50c0d74e91f5e64a3ec4eb9d54eb9f6
Antoan Metodiev <Boksnem@abv.bg>
2024-07-08 21:31:47 +0300

    create GameDetails

---
+++ client/src/components/Categories/Games/Ps5Games_Page/structure/Body/GameDetails/GameDetails.jsx
@@ -0,0 +1,17 @@
+import { Header } from "../../Header/Header";
+import { Body } from "./structure/Body/Body";
+
+export const GameDetails = () => {
+  
+
+  return (
+    <>
+       <Header />
+
+       <Body />
+      
+    </>
+  );
+};
+
+export default GameDetails;
