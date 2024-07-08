bcf3e5703f27ab28a0f1830c44fc7fa3b0e96bc6
Antoan Metodiev <Boksnem@abv.bg>
2024-07-08 21:34:32 +0300

    create Header component in GameDetails

---
+++ client/src/components/Categories/Games/Ps5Games_Page/structure/Body/GameDetails/structure/Header/Header.jsx
@@ -0,0 +1,23 @@
+
+import style from "./Header.module.css";
+
+export const Header = () => {
+
+    return (
+        <header className={styles['site-header']}>
+            <div className={styles['site-logo-name-container']}>
+                <img className={styles['site-logo']} src={site_logo} alt="site-logo" />
+                <h1 className={styles['site-name']}>GalaxyPlay</h1>
+            </div>
+
+            <form>
+                <input type="text" placeholder="What will you search for today?"/>
+                <button className={styles['search-button']}>Search</button>
+            </form>
+
+            <div className={styles['user-container']}>
+                <h2>User1</h2>
+            </div>
+        </header>
+    );
+}
