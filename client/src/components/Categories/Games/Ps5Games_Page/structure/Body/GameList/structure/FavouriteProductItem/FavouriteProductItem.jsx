5a704cf2a6db86e5938c14a19ebf372fe9923463
Antoan Metodiev <Boksnem@abv.bg>
2024-07-22 04:15:27 +0300

    create FavouriteProductItem for section with all of this

---
+++ client/src/components/Categories/Games/Ps5Games_Page/structure/Body/GameList/structure/FavouriteProductItem/FavouriteProductItem.jsx
@@ -0,0 +1,44 @@
+
+import { useLocation, useNavigate } from "react-router-dom";
+import style from "../FavouriteProductItem/FavouriteProductItem.module.css";
+
+export const FavouriteProductItem = ({
+    product,
+}) => {
+
+    let location = useLocation();
+    let navigate = useNavigate();
+    function NavigateToProduct() {
+        debugger;
+
+        console.log(product);
+        let arrPath = product.path.split('/');
+        arrPath.shift();
+        debugger;
+        let currentPath = '';
+        if (arrPath[1] === 'games') {
+            currentPath = `/${arrPath[0]}/game/${arrPath[2]}/${arrPath[3]}`; 
+            navigate(currentPath, { replace: true });
+        } else {
+            currentPath = `/${arrPath[0]}/${arrPath[1]}/details/${arrPath[2]}`; 
+            navigate(currentPath, { replace: true });
+        }
+    };
+
+    debugger;
+    console.log('Az bqh vutre vuv FavouriteProductItem ');
+
+    return (
+        <div className={style['favourite-product-item-container']}>
+
+            <img className={style['product-image']} src={product.productImage} alt="product-image" />
+
+            <div className={style['product-content']}>
+                <h2 className={style['product-title']}>{product.productName}</h2>
+
+                <p className={style['price']}>{product.productPrice} USD</p>
+                <button onClick={NavigateToProduct} className={style['view-button']}>View</button>
+            </div>
+        </div>
+    );
+}
