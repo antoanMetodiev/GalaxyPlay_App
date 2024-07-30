be3b400076f11bbfe39708fa593195b89e60f6c4
Antoan Metodiev <Boksnem@abv.bg>
2024-07-30 04:12:50 +0300

    add dynamic search about Chat component (he can search dynamicly while typing in input element)

---
+++ client/src/components/Chat/AllChats/structure/SearchEngine/SearchEngine.jsx
@@ -0,0 +1,31 @@
+
+import style from "../../../AllChats/AllChats.module.css";
+
+export const SearchEngine = ({
+    allChatUsers,
+    setFilteredUsersHandler,
+}) => {
+
+
+    function filterUsers(event) {
+        let filteredObject = {};
+
+        debugger;
+        for (const key in allChatUsers) {
+            if (key.toLowerCase().includes(event.currentTarget.value)) {
+                filteredObject[key] = allChatUsers[key];
+            }
+        }
+
+        setFilteredUsersHandler(filteredObject);
+    }
+
+
+    return (
+        <input
+            onChange={filterUsers}
+            placeholder="Search..."
+            className={style['search-engine']}
+            type="text" />
+    );
+}
