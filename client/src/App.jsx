799422d1cca703ed893bd1cfe8cd84e352866cff
Antoan Metodiev <Boksnem@abv.bg>
2024-08-07 23:30:38 +0300

    rename file and ordered my code and compress images

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,27 @@ import { DiscoverPage } from "./components/DiscoverPage/DiscoverPage";
 import { Register } from "./components/Register-Login_Page/Register/Register";
 import { Login } from "./components/Register-Login_Page/Login/Login";
 import { Routes, Route, useLocation } from "react-router-dom";
-import { useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 
 import { Categories } from "./components/Categories/Categories";
-import { Game_Categories } from "./components/Categories/Games/Game_Categories";
-import { Ps5Games_Page } from "./components/Categories/Games/Ps5Games_Page/Ps5Games_Page";
-import { GameDetails } from "./components/Categories/Games/Ps5Games_Page/structure/Body/GameDetails/GameDetails";
+import { Game_Categories } from "./components/Categories/Products/Game_Categories";
+import { SpecificCategory } from "./components/Categories/Products/SpecificCategory/SpecificCategory";
+import { ProductDetails } from "./components/Categories/Products/SpecificCategory/structure/Body/ProductDetails/ProductDetails";
 import { AllChats } from "./components/Chat/AllChats/AllChats";
 import { WithoutPermission } from "./components/WithoutPermission/WithoutPermission";
 import { BigImage } from "./components/BigImage/BigImage";
 
-import AudioPlayer from "./AudioPlayer";
+import { allAvatars, allAvatarsReversed } from "./utils/allAvatars";
+
+// import AudioPlayer from "./AudioPlayer";
 import { UserDetails } from "./UserDetails/UserDetails";
 import { InvalidPath } from "./components/InvalidPath/InvalidPath";
 import { GameReviews } from "./components/GameReviews/GameReviews";
 
+import Cookies from "js-cookie";
+import { AllAvatarsContext } from "./contexts/allAvatarsContext";
+
 function App() {
-	let [firstRender, setFirstRender] = useState(true);
 	const [userData, setUserData] = useState({});
 	let [logStatus, setLogStatus] = useState(false);
 	let location = useLocation();
@@ -26,17 +30,16 @@ function App() {
 	// References:
 	let bigImageRef = useRef(null);  // this is wrapper on bigImageRef, you should remove name!!!
 	let currentBigImageRef = useRef(null);
+	let pathName = useRef('');
 
-	// debugger;
-	useEffect(() => {
-		if (JSON.parse(localStorage.getItem('user'))) {
-			setLogStatus(true);
-			setFirstRender(false);
-		}
 
-	}, [location.pathname, JSON.parse(localStorage.getItem('user'))]);
+	function removeLogStatus() {
+		setLogStatus(false);
+	}
+
 
 	const setUserDataHandler = (newData) => {
+		setLogStatus(true);
 		setUserData(newData);
 	};
 
@@ -47,87 +50,109 @@ function App() {
 		currentBigImageRef.current.src = imagePath;
 	}
 
+	useEffect(() => {
+		// Проверка на cookie при зареждане на приложението
+		const token = Cookies.get('session');
+
+		if (token) {
+			setLogStatus(true);
+		} else {
+			setLogStatus(false);
+		}
+
+	}, []); // Празен масив за да се изпълни само при зареждане на компонента
 
 
-	let exp = localStorage.getItem('user');
-	exp = exp == null ? undefined : exp;
+
 	return (
-		<>
-			{/* <AudioPlayer /> */}
 
-			{logStatus && (
-				<>
-					<AllChats
-						currentBigImageRef={currentBigImageRef}
-						showBigImage={showBigImage}
-						bigImageRef={bigImageRef}
-					/>
+		<AllAvatarsContext.Provider value={{ 'allAvatars': allAvatars, 'allAvatarsReversed': allAvatarsReversed }}>
+			<>
+				{/* <AudioPlayer /> */}
 
-					<BigImage
-						currentBigImageRef={currentBigImageRef}
-						bigImageRef={bigImageRef}
-					/>
-				</>
-			)}
-
-			<Routes>
-				<Route path="/" element={<DiscoverPage />} />
-				<Route path="/register" element={<Register />} />
-				<Route
-					path="/login"
-					element={<Login setUsDataHandler={setUserDataHandler} />}
-				/>
 
 				{logStatus && (
 					<>
-						<Route path="/categories" element={<Categories />} />
-						<Route path="/categories/games" element={<Game_Categories />} />
-						<Route path="/profile-details" element={<UserDetails />} />
-
-						{/* Example: */}
-
-						<Route
-							path="/categories/:specificCategory/:subcategory?"
-							element={<Ps5Games_Page />}
+						<AllChats
+							currentBigImageRef={currentBigImageRef}
+							showBigImage={showBigImage}
+							bigImageRef={bigImageRef}
 						/>
 
-						{/* For All Game Categories: */}
-						<Route
-							path="/categories/game/:subcategory/:gameId"
-							element={<GameDetails />}
+						<BigImage
+							currentBigImageRef={currentBigImageRef}
+							bigImageRef={bigImageRef}
 						/>
-
-
-						{/* For Others Categories without subCategory: */}
-						<Route
-							path="/categories/:specificCategory/details?/:gameId"
-							element={<GameDetails />}
-						/>
-
-
-						{/* Game Rewiews */}
-						<Route path="/game-reviews" element={<GameReviews />} />
-
 					</>
 				)}
 
-				{logStatus && (
-					<>
+				<Routes>
+
+					<Route path="/" element={<DiscoverPage logStatus={logStatus} removeLogStatus={removeLogStatus} />} />
+
+					{!logStatus && (
+						<>
+							<Route path="/register" element={<Register setUserDataHandler={setUserDataHandler} />} />
+							<Route
+								path="/login"
+								element={<Login setUserDataHandler={setUserDataHandler} />}
+							/>
+						</>
+					)}
+
+					{logStatus && (
+						<>
+							<Route path="/categories" element={<Categories />} />
+							<Route path="/categories/games" element={<Game_Categories />} />
+							<Route path="/profile-details" element={<UserDetails />} />
+
+							{/* Example: */}
+
+							<Route
+								path="/categories/:specificCategory/:subcategory?"
+								element={<SpecificCategory />}
+							/>
+
+							{/* For All Game Categories: */}
+							<Route
+								path="/categories/game/:subcategory/:gameId"
+								element={<ProductDetails />}
+							/>
+
+
+							{/* For Others Categories without subCategory: */}
+							<Route
+								path="/categories/:specificCategory/details?/:gameId"
+								element={<ProductDetails />}
+							/>
+
+
+							{/* Game Rewiews */}
+							<Route path="/game-reviews" element={<GameReviews />} />
+
+						</>
+					)}
+
+					{!logStatus && !Cookies.get('session') && (
 						<Route
 							path="*"
-							element={<InvalidPath />}
+							element={<WithoutPermission />}
 						/>
-					</>
-				)}
+					)}
 
-				{!logStatus && !exp && (
-					<Route
-						path="*"
-						element={<WithoutPermission />}
-					/>
-				)}
-			</Routes>
-		</>
+
+					{logStatus && (location.pathname !== '/register' && location.pathname !== '/login'
+						&& location.pathname !== '/' && location.pathname !== '') && (
+							<>
+								<Route
+									path="*"
+									element={<InvalidPath />}
+								/>
+							</>
+						)}
+				</Routes>
+			</>
+		</AllAvatarsContext.Provider>
 	);
 }
 
