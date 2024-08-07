2f29153a025ecbb25e22679032ba056d2c07b340
Antoan Metodiev <Boksnem@abv.bg>
2024-08-07 23:19:09 +0300

    rename components

---
+++ client/src/components/Categories/Products/SpecificCategory/structure/Body/ProductDetails/structure/Body/structure/GameDescription/GameDescription.jsx
@@ -0,0 +1,56 @@
+import style from "./GameDescription.module.css";
+import { Link } from "react-scroll";
+import ReactPlayer from "react-player";
+
+import { GameCharacteristics } from "./structure/GameCharacteristics/GameCharacteristics";
+
+export const GameDescription = ({ gameDetails }) => {
+	
+	
+
+	return (
+		<>
+			{gameDetails && (
+				<>
+					<h2 className={style["full-product-desk-title"]}>
+						FULL PRODUCT DESCRIPTION
+					</h2>
+					<p className={style["desc-text"]}>{gameDetails.description[0]}</p>
+					<img
+						className={style["desc-image"]}
+						src={gameDetails.otherImageUrl[0]}
+						alt={gameDetails.name + " image"}
+					/>
+					<p className={style["desc-text"]}>{gameDetails.description[1]}</p>
+					<img
+						className={style["desc-image"]}
+						src={gameDetails.otherImageUrl[1]}
+						alt={gameDetails.name + " image"}
+					/>
+					<p className={style["desc-text"]}>{gameDetails.description[2]}</p>
+					<img
+						className={style["desc-image"]}
+						src={gameDetails.otherImageUrl[2]}
+						alt={gameDetails.name + " image"}
+					/>
+
+					<p className={style["desc-text"]}>{gameDetails.description[3]}</p>
+
+					<h2 className={style["trailer-title"]}>
+						{gameDetails.name} - Trailer
+					</h2>
+					<div className={style["video-wrapper-container"]} id="trailer_id">
+						<ReactPlayer
+							url={gameDetails.trailer}
+							width="100%"
+							height="100%"
+							controls
+						/>
+					</div>
+
+					<GameCharacteristics />
+				</>
+			)}
+		</>
+	);
+};
